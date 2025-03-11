15211fb21dc2f72499756632743a3aa7e7d869d2
Rasmus Eng <rasmus.eng@elev.ga.ntig.se>
2025-03-11 10:08:06 +0100

    start

--- Prov1December/obj/Debug/net8.0/Prov1December.AssemblyInfo.cs
+++ Prov1December/obj/Debug/net8.0/Prov1December.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prov1December")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90a7d5a2690a085c387869fe29e5edc1a8685587")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daf3c4f2007616d903f3c1d3f50ba3b31be68d94")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prov1December")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prov1December")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
